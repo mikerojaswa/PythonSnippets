4d83b089393db8a597dfa2c88a7d22063224f6e0
Michael Rojas <mikerojaswa@gmail.com>
2018-12-10 16:23:21 -0700

    merge sort

--- Sorting/merge.py
+++ Sorting/merge.py
@@ -1,11 +1,30 @@
-def merge_sort(arr):
-	if len(arr) == 1:
-		return arr
+def merge_sort(array):
+	if len(array) <= 1:  # base case
+		return array
+
+	# divide array in half and merge sort recursively
+	half = len(array) // 2
+	left = merge_sort(array[:half])
+	right = merge_sort(array[half:])
+
+	return merge(left, right)
+def merge(left, right):
+	arr = []
+	l = 0
+	r = 0
+	while l < len(left) and r < len(right):
+		if left[l] < right[r]:
+			arr.append(left[l])
+			l += 1
+		else:
+			arr.append(right[r])
+			r += 1
+	arr = arr + left[l:len(left)]
+	arr = arr + right[r:len(right)]
 	
-	a = arr[0:len(arr)/2]
-	b = arr[(len(arr)/2) + 1: len(arr)]
+	return arr
 	
 	
 arr = [5,4,3,2,1]
-merge_sort(arr)
+arr = merge_sort(arr)
 print(arr)
