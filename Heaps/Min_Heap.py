7227ad54283aaad580c6ed354dd195ec0f0069fb
Michael Rojas <mikerojaswa@gmail.com>
2019-02-07 22:34:36 -0700

    Ordering

--- Heaps/Min_Heap.py
+++ Heaps/Min_Heap.py
@@ -28,3 +28,25 @@ tuple_node_arr = [(item.val, item) for item in node_arr]
 
 heapq.heapify(tuple_node_arr)
 print(heapq.heappop(tuple_node_arr)) # pops -4440 node
+
+# --------------------------------
+# 2
+# heapify with a custom data structure 
+# using a tuple as priority with equality for sorting supported
+# --------------------------------
+
+class OrderableNode:
+	def __init__(self, val):
+		self.val = val
+	def __lt__(self, other):
+		return (self.val < other.val)
+node_arr = [OrderableNode(5), OrderableNode(8), OrderableNode(8), OrderableNode(9)]
+
+tuple_node_arr = [(item.val, item) for item in node_arr] 
+
+# because we have two nodes with value 8 in tuple we need to define a __lt__ on the node class
+# that enables comparison
+print(heapq.heappop(tuple_node_arr)) # 5
+print(heapq.heappop(tuple_node_arr)) # 8
+print(heapq.heappop(tuple_node_arr)) # 8
+print(heapq.heappop(tuple_node_arr)) # 9
